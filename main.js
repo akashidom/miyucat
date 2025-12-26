d1aa403b878351781766226e5c6949572d33f958
akashidom <akashidomon@gmail.com>
2025-12-26 22:57:17 +0800

    Add main.js with some imports and enviroments in it

---
+++ main.js
@@ -0,0 +1,8 @@
+// IMPORTS
+import { REST, Routes } from 'discord.js'
+import 'dotenv/config'
+
+// load enviromentals
+const token = process.env.CLIENT_TOKEN
+const clientId = process.env.CLIENT_ID
+
