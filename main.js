d0ddf9891254bf7eba162e0211c492ef68d417f1
akashidom <akashidomon@gmail.com>
2025-12-26 23:59:06 +0800

    Paste from https://discord.js.org/docs/packages/discord.js/14.25.1 to main.js

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 // IMPORT
-import { REST, Routes } from 'discord.js';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 import 'dotenv/config';
 
 // load env
 const TOKEN = process.env.CLIENT_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on(Events.ClientReady, readyClient => {
+  console.log(`Logged in as ${readyClient.user.tag}!`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
 await import('./command.js');
+
+client.login(TOKEN);
+
