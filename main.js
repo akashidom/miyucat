b8cf0ce8d6bcff216f56791af59b49ff85cf8f76
akashidom <akashidomon@gmail.com>
2025-12-27 02:24:34 +0800

    Trying to merge main.js and command.js

--- main.js
+++ main.js
@@ -14,6 +14,29 @@ client.on(Events.ClientReady, readyClient => {
   console.log(`===★★★ Logged in as ${readyClient.user.tag}! ★★★===}`);
 });
 
+// declare commands
+const commands = [];
+const files = await readdir('./slash-commands/').filter(file => file.endsWith('.js'));
+
+// push commands
+for (const file of files) {
+  const command = await import(`./slash-commands/${file}`);
+  commands.push(command.default.data.toJSON());
+}
+
+// declare rest
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+try {
+  console.log('# Started refreshing application (/) commands…');
+  if (DEBUG_MODE) console.log('>>> Commands:', commands);
+  
+  // refresh app (/) commands
+  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+  
+  console.log('@ Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error('>@>@>@>@>@ Error occured while refreshing application (/) commands.', error);
+}
 // when app (/) command sent 
 client.on(Events.InteractionCreate, async interaction => {
   if (!interaction.isChatInputCommand()) return;
