f4d5e6358f55166735a333f6280304da05ff855d
akashidom <akashidomon@gmail.com>
2025-12-26 23:21:40 +0800

    Capitalize important variables in main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ import { REST, Routes } from 'discord.js'
 import 'dotenv/config'
 
 // load enviromentals
-const token = process.env.CLIENT_TOKEN
-const clientId = process.env.CLIENT_ID
+const TOKEN = process.env.CLIENT_TOKEN
+const CLIENT_ID = process.env.CLIENT_ID
 
