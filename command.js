1201bf00722972079a70ea641a77ffa4bb51975b
akashidom <akashidomon@gmail.com>
2025-12-27 02:21:13 +0800

    Improve comments and logging to be more verbose.

--- command.js
+++ command.js
@@ -2,28 +2,32 @@
 import { REST, Routes } from 'discord.js';
 import { readdir } from 'node:fs/promises'
 import 'dotenv/config';
-console.log('@ Imported command.js')
+console.log('@ Imported command.js.')
 
-// load env
+// load .env
+const DEBUG_MODE = process.env.DEBUG_MODE === "true";
 const TOKEN = process.env.CLIENT_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 
+// declare commands
 const commands = [];
 const files = await readdir('./slash-commands/').filter(file => file.endsWith('.js'));
 
+// push commands
 for (const file of files) {
   const command = await import(`./slash-commands/${file}`);
   commands.push(command.default.data.toJSON());
 }
 
-const rest = new REST({ version: '10' }).setToken(TOKEN);
-
+const rest = new REST({ version: '10' }).setToken(TOKEN); // declare rest
 try {
-  console.log('# Started refreshing application (/) commands.');
-
+  console.log('# Started refreshing application (/) commands…');
+  if (DEBUG_MODE) console.log('>>> Commands:', commands);
+  
+  // refresh app commands
   await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-
+  
   console.log('@ Successfully reloaded application (/) commands.');
 } catch (error) {
-  console.error(error);
+  console.error('>@>@>@>@>@ Error occured while refreshing application (/) commands.', error);
 }
--- main.js
+++ main.js
@@ -1,30 +1,34 @@
 // IMPORT
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 import 'dotenv/config';
-console.log('@ Imported main.js')
+console.log('@ Imported main.js.')
 
-// load env
-const DEBUG_MODE = process.env.DEBUG_MODE;
+// load .env
+const DEBUG_MODE = process.env.DEBUG_MODE === "true";
 const TOKEN = process.env.CLIENT_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 
+// declare client
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
 client.on(Events.ClientReady, readyClient => {
-  console.log(`Logged in as ${readyClient.user.tag}!`);
+  console.log(`===★★★ Logged in as ${readyClient.user.tag}! ★★★===}`);
 });
 
+// when app (/) command sent 
 client.on(Events.InteractionCreate, async interaction => {
   if (!interaction.isChatInputCommand()) return;
-    if (DEBUG_MODE) console.log("[SLASH]", interaction.user.tag, '→', interaction.commandName);
+    if (DEBUG_MODE) console.log("[/]", interaction.user.tag, '→', interaction.commandName);
 
   if (interaction.commandName === 'ping') {
     await interaction.reply('Pong!');
   }
 });
 
-console.log("# Importing command.js")
+// import commands
+console.log("# Importing command.js.")
 await import('./command.js');
 
+/* LOG IN */
+console.log("= Logging in…")
 client.login(TOKEN);
 
