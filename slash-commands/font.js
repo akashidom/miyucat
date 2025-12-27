5f503ba62446790f0fad300e3b4b99b6219b5a5b
akashidom <akashidomon@gmail.com>
2025-12-27 10:47:56 +0800

    Add message and font option to font.js

--- slash-commands/font.js
+++ slash-commands/font.js
@@ -7,6 +7,15 @@ import {
 export default {
   data: new SlashCommandBuilder()
   .setName('font')
-  .setDescription('Replace font whatever text you sent ;) to the one you chose'),
+  .setDescription('Replace font whatever text you sent ;) to the one you chose')
+  .addStringOption(option => option
+  .setName('message')
+  .setDescription('Enter message you want to apply the font :D')
+  .setRequired(true))
+  .addStringOption(option => option
+  .setName('font')
+  .setDescription('Pick the font you want to apply ;)')
+  .addChoices()
+  .setRequired(true)),
   async execute(interaction, DEBUG_MODE) {}
 };
