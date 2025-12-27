28f1d47ac59e9cdc503f7b2bacf566ac5f15c83c
akashidom <akashidomon@gmail.com>
2025-12-28 03:29:19 +0800

    Format miku.js

--- slash-commands/miku.js
+++ slash-commands/miku.js
@@ -1,15 +1,17 @@
 import {
   SlashCommandBuilder
 } from 'discord.js';
-import { search } from 'booru';
+import {
+  search
+} from 'booru';
 
 export default {
   data: new SlashCommandBuilder()
   .setName('miku')
   .setDescription('Shows random miku in the chat /owo\\')
   .addBooleanOption(option => option
-  .setName("sensitive")
-  .setDescription("with slightly more skin ;)")
+    .setName("sensitive")
+    .setDescription("with slightly more skin ;)")
   ),
   async execute(interaction, DEBUG_MODE) {
     await interaction.deferReply();
@@ -17,19 +19,19 @@ export default {
     if (interaction.options.getBoolean('sensitive')) {
       rating = 'rating:sensitive';
     }
-    
+
     let post;
     do {
-    const posts = await search('danbooru', ['hatsune_miku', rating], {
-      limit: 1,
-      random: true
-    })
+      const posts = await search('danbooru', ['hatsune_miku', rating], {
+        limit: 1,
+        random: true
+      })
       post = posts[0];
-      
-    if (DEBUG_MODE) console.log('>>> Post:', post)
+
+      if (DEBUG_MODE) console.log('>>> Post:', post)
     } while (!post.fileUrl || post.fileUrl.endsWith('.mp4'))
-    
-    await interaction.editReply({
+
+      await interaction.editReply({
       content: post.fileUrl
     })
   }
