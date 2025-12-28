2e4c9f9db6d57a05c203b147fbf793eddd7427a4
akashidom <akashidomon@gmail.com>
2025-12-28 21:20:33 +0800

    Move execute inside a function in miku.js

--- slash-commands/miku.js
+++ slash-commands/miku.js
@@ -5,6 +5,38 @@ import {
   search
 } from 'booru';
 
+async function reply(interaction, DEBUG_MODE, tags = []) {
+  tags = Array.isArray(tags) ? tags: [tags];
+  let flags = 0;
+  let rating = interaction.options.getString('rating');
+  if (!rating) {
+    rating = 'general';
+  } else if (rating === 'questionable,explicit') {
+    // note : no need to check if it's dm since if it's not in channel it'll flags 64 anyway
+    if (!interaction.channel?.nsfw || !interaction.channel) {
+      flags = 64;
+    }
+  }
+  await interaction.deferReply({
+    flags: flags
+  });
+
+  let post;
+  do {
+    const posts = await search('danbooru', [...tags, 'rating:' + rating], {
+      limit: 1,
+      random: true
+    })
+    post = posts[0];
+
+    if (DEBUG_MODE) console.log('>>> Post:', post)
+  } while (!post?.fileUrl || /\.(mp4|zip|webm)$/i.test(post.fileUrl ?? ''))
+
+    await interaction.editReply({
+    content: post.fileUrl
+  })
+}
+
 export default {
   data: new SlashCommandBuilder()
   .setName('miku')
@@ -23,33 +55,6 @@ export default {
       }])
   ),
   async execute(interaction, DEBUG_MODE) {
-    let flags = 0;
-    let rating = interaction.options.getString('rating');
-    if (!rating) {
-      rating = 'general';
-    } else if (rating === 'questionable,explicit') {
-      // note : no need to check if it's dm since if it's not in channel it'll flags 64 anyway
-      if (!interaction.channel?.nsfw || !interaction.channel) {
-        flags = 64;
-      }
-    }
-    await interaction.deferReply({
-      flags: flags
-    });
-
-    let post;
-    do {
-      const posts = await search('danbooru', ['hatsune_miku', 'rating:' + rating], {
-        limit: 1,
-        random: true
-      })
-      post = posts[0];
-
-      if (DEBUG_MODE) console.log('>>> Post:', post)
-    } while (!post.fileUrl || /\.(mp4|zip|webm)$/i.test(post.fileUrl ?? ''))
-
-      await interaction.editReply({
-      content: post.fileUrl
-    })
+    return reply(interaction, DEBUG_MODE, 'hatsune_miku');
   }
 }
