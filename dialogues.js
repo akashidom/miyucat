1ef5b0e76ab67983867b626f75349a116cc87b5d
akashidom <akashidomon@gmail.com>
2026-01-11 07:10:27 +0800

    Add dialogues file to store all miyucat random dialogues

---
+++ dialogues.js
@@ -0,0 +1,21 @@
+export const salutations = [
+'hi',
+'hi',
+'hey',
+'hey',
+'hello',
+'hello',
+'wsup',
+'greeting',
+'salutation'
+];
+
+export const expressions = [
+':3',
+'^w^',
+'0w0',
+'—w—',
+';3',
+'=3',
+'~(^•w•^)'
+];
