443890a762989cc010b66f7da625a4dc1cf51f6a
sha <akashidomon@gmail.com>
2026-02-02 00:27:44 +0800

    Cast DISCORD_TOKEN to str

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-token: str = os.getenv("DISCORD_TOKEN")
+token: str = str(os.getenv("DISCORD_TOKEN"))
 
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 intents = discord.Intents.default()
