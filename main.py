3ecde734fa9450c5fcece756b260a98c70aec4f6
sha <akashidomon@gmail.com>
2026-02-02 00:10:42 +0800

    Add Discord intents and logging setup
    Add a file-based log handler and enable message_content and member
    intents

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from dotenv import load_dotenv
+import logging
 import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
+
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
